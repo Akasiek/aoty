4c98a40d2369588a1c61a39249e5b85113fbe550
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2021-12-26 15:25:36 +0100

    Added links to all users whom created their list. For easy editing

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Album of the Year - Startpage</title>
+
+    <style>
+        section.user_choice {
+            display: flex;
+        }
+
+        section.user_choice>div {
+            flex: 1;
+            padding: 10px 20px;
+        }
+    </style>
 </head>
 
 <body>
@@ -17,39 +28,51 @@
 
     <h1>Album of the year. Choose yours!</h1>
 
-    <div>
-        <form action="create_list.php" method="post">
-            <h3>Create list</h3>
-            <p>Choose your account:
-                <select name="username_input" id="username_input">
-                    <option value=""></option>
-                    <?php
-                    $query = mysqli_query($mysql_conn, 'SELECT * FROM users');
-                    while ($row = mysqli_fetch_array($query))
-                        echo "<option value=\"" . $row[1] . "\">" . $row[1] . "</option>";
-                    ?>
-                </select>
-            </p>
-            <input type="submit" value="Create your list">
-        </form>
-    </div>
+    <section class="user_choice">
+        <div>
+            <form action="list_creator.php" method="get">
+                <h3>Create list</h3>
+                <p>Choose your account:
+                    <select name="username_input" id="username_input">
+                        <option value=""></option>
+                        <?php
+                        $query = mysqli_query($mysql_conn, 'SELECT * FROM users');
+                        while ($row = mysqli_fetch_array($query))
+                            echo "<option value=\"" . $row[1] . "\">" . $row[1] . "</option>";
+                        ?>
+                    </select>
+                </p>
+                <input type="submit" value="Create your list">
+            </form>
+        </div>
+
+        <hr>
+
+        <div>
+            <h3>Users, whom created their list</h3>
+            <?php
+            $result = "";
+            $query = mysqli_query($mysql_conn, 'SELECT users.username FROM lists INNER JOIN users ON lists.owner_id = users.id');
+            if (mysqli_num_rows($query) == 0) {
+                print "No users yet.";
+            } else {
+                while ($row = mysqli_fetch_array($query)) {
+                    $user_markup = "<a href='list_creator.php?username_input=" . $row[0] . "'>" . $row[0] . "</a>";
+                    $result .= $user_markup . ", ";
+                }
+            }
+            print substr($result, 0, -2);
+            ?>
+        </div>
+    </section>
 
     <hr>
 
-    <div>
-        <h3>Users, whom created their list</h3>
-        <?php
-        $result = "";
-        $query = mysqli_query($mysql_conn, 'SELECT users.username FROM lists INNER JOIN users ON lists.owner_id = users.id');
-        if (mysqli_num_rows($query) == 0) {
-            print "No users yet.";
-        } else {
-            while ($row = mysqli_fetch_array($query))
-                $result .= $row[0] . ", ";
-        }
-        print substr($result, 0, -2);
-        ?>
-    </div>
+    <section class="leaderboard">
+        <h1 style="text-align:center;">Leaderboard</h1>
+
+    </section>
+
 
 </body>
 
